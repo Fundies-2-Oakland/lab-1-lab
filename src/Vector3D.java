434644de0bf708b832e538b55d9ae54ecfd18f20
Anderson Liew <alacc776@gmail.com>
2025-01-10 13:12:06 -0800

    First 2 questions

---
+++ src/Vector3D.java
@@ -0,0 +1,32 @@
+package src;
+
+public class Vector3D {
+
+    // QUESTION 1
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3D(double nX, double nY, double nZ) {
+        this.x = nX;
+        this.y = nY;
+        this.z = nZ;
+    }
+
+    // QUESTION 2
+    private double getX() {
+        return x;
+    }
+
+    private double getY() {
+        return y;
+    }
+
+    private double getZ() {
+        return z;
+    }
+
+    // QUESTION 3
+
+
+}
